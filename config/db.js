b82e6b7b29be4ee1562d1b1decf31c4a82b77b0e
benfro21 <broad0601@gmail.com>
2023-01-12 14:14:05 -0500

    started ctrl

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
+// 'mongodb+srv://benfro21:123Benjamin1257@cocktails.w9p5zuu.mongodb.net/cocktails?retryWrites=true&w=majority'
 
-
-mongoose.connect('mongodb://localhost:27017/toddlerToys', {
+mongoose.connect('mongodb://localhost:27017/toddler_toys', {
     useNewUrlParser: true,
-    // useFindAndModify: false,
-    useUnifiedTopology: true
-})
-
-
+    useUnifiedTopology: true,
+  });
+  
 
 const db = mongoose.connection
 
-db.on('Connected', () => {
-    console.log(`Connected to mongoDb at ${db.host}:${db.port }`)
+db.on('connected', () => {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
 })
--- controllers/toyCtrl.js
+++ controllers/toyCtrl.js
@@ -1,1 +1,11 @@
 const Toy = require('../models/toyModel')
+
+
+let showAll = (req,res) => {
+    Toy.find({})
+    if(err){
+        res.status(400).json(err)
+        return
+    }
+    res.json()
+}
